f0a1d76844e5188ecdfbca962f547dce9735bda5
c_ringwald <42188996+datalogism@users.noreply.github.com>
2022-03-13 18:53:13 +0100

    Update download_light.sh

--- light_app/download_light.sh
+++ light_app/download_light.sh
@@ -123,6 +123,7 @@ then
 else 
 	echo "[END PROCESS] data already up-to-date";
 fi  
-cp ./dbpedia_fr-metadata.ttl ${TARGET_DIR}/${newDIR}/dbpedia_fr-metadata.ttl
+mkdir ${TARGET_DIR}/${newDIR}/meta_base/
+cp ./dbpedia_fr-metadata.ttl ${TARGET_DIR}/${newDIR}/meta_base/dbpedia_fr-metadata.ttl
 rm "${TARGET_DIR}/download_locker.lck";
 echo "[END PROCESS] See you !";
